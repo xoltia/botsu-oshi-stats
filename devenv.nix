eab77f827bc93ae11fc59cb9a5e46bea558a459b
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-06-22 17:08:02 -0500

    Add watch script

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,7 @@
     templ
     tailwindcss_4
     tailwindcss-language-server
+    entr
   ];
   languages.go.enable = true;
   services.postgres.enable = true;
@@ -14,5 +15,20 @@
     tailwindcss -i input.css -o static/styles.css
     templ generate
     go build -o ./bin/server .
+  '';
+
+  scripts.build-run.exec = ''
+    build
+    BOTSU_DB_URL="postgresql:///botsu" ./bin/server "$@"
+  '';
+  
+  scripts.run-watch.exec = ''
+    find . \
+      \( -path '*/.*' -prune \) -o \
+      \( -type f \
+         \( -name '*.go'   ! -name '*_templ.go' \) \
+         -o -name '*.templ' \
+      \) -print \
+    | entr -cr build-run "$@"
   '';
 }
