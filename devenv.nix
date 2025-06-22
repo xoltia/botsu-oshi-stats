172e793398b3cab8228e989c359dda73cdff5762
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-06-21 21:53:41 -0500

    Add tailwindcss

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,18 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ pkgs.templ ];
+  packages = with pkgs; [
+    templ
+    tailwindcss_4
+    tailwindcss-language-server
+  ];
   languages.go.enable = true;
   services.postgres.enable = true;
   services.postgres.initialDatabases = [ { name = "botsu"; } ];
+
+  scripts.build.exec = ''
+    tailwindcss -i input.css -o static/output.css
+    templ generate
+    go build -o ./bin/server .
+  '';
 }
