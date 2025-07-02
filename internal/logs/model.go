d16797f0b2579e3df23bdd8beedc588d43996f3d
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-07-01 19:13:10 -0500

    Add logs repository

---
+++ internal/logs/model.go
@@ -0,0 +1,32 @@
+package videos
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type VideoInfo struct {
+	ID             string        `json:"video_id"`
+	Title          string        `json:"video_title"`
+	ChannelID      string        `json:"channel_id"`
+	ChannelHandle  string        `json:"channel_handle"`
+	LinkedChannels string        `json:"linked_channels"`
+	Duration       time.Duration `json:"video_duration"`
+}
+
+func (v *VideoInfo) Scan(value any) error {
+	data, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("expected byte array, got %T", value)
+	}
+	return json.Unmarshal(data, v)
+}
+
+type Log struct {
+	ID       int           `db:"id"`
+	UserID   string        `db:"user_id"`
+	Date     time.Time     `db:"date"`
+	Duration time.Duration `db:"duration"`
+	Video    VideoInfo     `db:"meta"`
+}
---
+++ internal/logs/repository.go
@@ -0,0 +1,47 @@
+package videos
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type UserLogRepository struct {
+	db *sqlx.DB
+}
+
+type LogSet struct {
+	rows *sqlx.Rows
+}
+
+func (s LogSet) Next() bool {
+	return s.rows.Next()
+}
+
+func (s LogSet) Err() error {
+	return s.rows.Err()
+}
+
+func (s LogSet) Scan() (Log, error) {
+	var log Log
+	err := s.rows.StructScan(&log)
+	return log, err
+}
+
+func (s LogSet) Close() error {
+	return s.rows.Close()
+}
+
+func (r *UserLogRepository) querySet(ctx context.Context, query string, args ...any) (logs LogSet, err error) {
+	rows, err := r.db.QueryxContext(ctx, query, args...)
+	logs = LogSet{rows: rows}
+	return
+}
+
+func (r *UserLogRepository) GetAll(ctx context.Context) (logs LogSet, err error) {
+	return r.querySet(ctx, `
+		SELECT id, user_id, date, duration, meta
+		FROM activities
+		WHERE media_type = 'video' AND meta->>platform = 'youtube';
+	`)
+}
